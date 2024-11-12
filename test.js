c032d23b1cbf89b0f5e54df0aaded7fa81f88df4
EuiJoong <euijki87@gmail.com>
2024-11-12 12:37:22 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
 import chalk from 'chalk';
 
-console.log(Math.floor(Math.random() * 100));
+// console.log(Math.floor(Math.random() * 500) + 1);
 
-console.log(chalk.black(`black 한글`));
-console.log(chalk.blackBright(`blackBright 한글`));
-console.log(chalk.blue(`blue 한글`));
-console.log(chalk.blueBright(`blueBright 한글`));
-// console.log(chalk.caller(`caller`));
-console.log(chalk.cyan(`cyan 한글`));
-console.log(chalk.cyanBright(`cyanBright 한글`));
-console.log(chalk.gray(`gray 한글`));
-console.log(chalk.green(`green 한글`));
-console.log(chalk.greenBright(`greenBright 한글`));
-console.log(chalk.grey(`grey 한글`));
-// console.log(chalk.hex(`hex`));
-console.log(chalk.magenta(`magenta 한글`));
-console.log(chalk.magentaBright(`magentaBright 한글`));
-console.log(chalk.yellow(`yellow 한글`));
-console.log(chalk.yellowBright(`YellowBright 한글`));
-console.log(chalk.white(`white 한글`));
-console.log(chalk.whiteBright(`whiteBright 한글`));
-console.log(chalk.bold(`bold 한글`));
+console.log(Math.floor(Math.random() * 100 + 1));
+
+// console.log(chalk.black(`black 한글`));
+// console.log(chalk.blackBright(`blackBright 한글`));
+// console.log(chalk.blue(`blue 한글`));
+// console.log(chalk.blueBright(`blueBright 한글`));
+// console.log(chalk.cyan(`cyan 한글`));
+// console.log(chalk.cyanBright(`cyanBright 한글`));
+// console.log(chalk.gray(`gray 한글`));
+// console.log(chalk.green(`green 한글`));
+// console.log(chalk.greenBright(`greenBright 한글`));
+// console.log(chalk.grey(`grey 한글`));
+// console.log(chalk.magenta(`magenta 한글`));
+// console.log(chalk.magentaBright(`magentaBright 한글`));
+// console.log(chalk.yellow(`yellow 한글`));
+// console.log(chalk.yellowBright(`YellowBright 한글`));
+// console.log(chalk.white(`white 한글`));
+// console.log(chalk.whiteBright(`whiteBright 한글`));
+// console.log(chalk.bold(`bold 한글`));
