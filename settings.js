5c38daf838c1282ad383851f4e708e0a0ffefdca
EuiJoong <euijki87@gmail.com>
2024-11-15 22:06:18 +0900

    Update settings.js
    설정 정보 수정

--- settings.js
+++ settings.js
@@ -4,11 +4,8 @@ export let maxCastleHp = 1000;
 /***
  * 스테이즈 설정
  */
-export let maxStage = 3;
-export let maxWave = 3;
-export let maxTurn = 5;
-
-//0: Easy 1: Nomal 2: Hard 3: Hell
-export let difficulty = 0;
+export let maxStage = 2;
+export let maxWave = 2;
+export let maxTurn = 3;
 
 export let useUnitCnt = 3;
